c3fcbcee86bd45ac90c8afed36b5d4adb9d8461b
ok <przywarty.it@gmail.com>
2018-04-30 10:35:12 +0200

    Add footer.

--- src/view/LaunchDetails.js
+++ src/view/LaunchDetails.js
@@ -144,6 +144,20 @@ class LaunchDetails extends React.Component {
               <a href={this.props.launch.links['video_link']}>MISSION VIDEO</a>
           </div>
         </div>
+        <div className="launch-details__footer">
+          <div className="footer__title">
+            <h6>FOLLOW SPACEX</h6>
+            <div className="footer__links">
+              <a href="https://twitter.com/spacex">TWITTER</a>
+              <a href="https://www.youtube.com/user/spacexchannel">YOUTUBE</a>
+              <a href="https://www.flickr.com/photos/spacex/">FLICKR</a>            
+              <a href="https://www.instagram.com/spacex/">INSTAGRAM</a>
+            </div>
+          </div>
+          <div className="footer__signature">
+            <h6>2018 SPACE EXPLORATION TECHNOLOGIES CORP.</h6>
+          </div>
+        </div>
       </div>
     );
   }
