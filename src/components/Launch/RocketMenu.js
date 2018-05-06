79baced1c7bec1f39d53266fea66ec0e5bd4b269
ok <przywarty.it@gmail.com>
2018-05-06 11:46:34 +0200

    Use BEM in RocketMenu.

--- src/components/Launch/RocketMenu.js
+++ src/components/Launch/RocketMenu.js
@@ -11,14 +11,17 @@ class RocketMenu extends React.Component {
         let backgroundImage = require('../../assets/rocket.png');
         return (
             <div className="rocket-menu" style={{backgroundImage: `url(${backgroundImage})`}}>
-                <img src={backgroundImage}/>
-                <div className="rocket-menu-title">
-                    <h2>MISSION LINKS</h2>
-                </div>
-                <div className="rocket-menu-body">
-                    <a href={this.props.launch.links['reddit_launch']}>REDDIT CAMPAIGN</a>
-                    <a href={this.props.launch.links['presskit']}>PRESSKIT</a>
-                    <a href={this.props.launch.links['video_link']}>MISSION VIDEO</a>
+                <h2>MISSION LINKS</h2>
+                <div className="rocket-menu__body">
+                    <a href={this.props.launch.links['reddit_launch']}>
+                        REDDIT CAMPAIGN
+                    </a>
+                    <a href={this.props.launch.links['presskit']}>
+                        PRESSKIT
+                    </a>
+                    <a href={this.props.launch.links['video_link']}>
+                        MISSION VIDEO
+                    </a>
                 </div>
             </div>
         );
