23e9141de2c7386bbbeb65dc37f6b440c38aa76e
ok <przywarty.it@gmail.com>
2018-05-06 12:52:36 +0200

    Add BEM to description details.

--- src/components/Launch/LaunchDescriptionEntry.js
+++ src/components/Launch/LaunchDescriptionEntry.js
@@ -13,23 +13,21 @@ class LaunchDescriptionEntry extends React.Component {
     render() {
         return (
             <div>
-                <div className="description-title">
+                <div className="description__title">
                     <h5>{this.props.descriptionTitle}</h5>
                 </div>
-                <div className="description-body">
+                <div className="description__body">
                     {this.props.descriptionDetailsList &&
-                        <div className="description-details">
+                        <div className="description__details">
                             {this.props.descriptionDetailsList.map(function (entry, i) {
-                                return <div className="details-entry" key={i}>
-                                    <span className="detail-key">{Object.keys(entry)[0]}: </span>
-                                    <span className="detail-value">{entry[Object.keys(entry)[0]]}</span>
+                                return <div className="description__details__entry" key={i}>
+                                    <span className="description__details__entry__key">{Object.keys(entry)[0]}: </span>
+                                    <span>{entry[Object.keys(entry)[0]]}</span>
                                 </div>;
                             })}
                         </div>
                     }
-                    <p>
-                        {this.props.description}
-                    </p>
+                    <p>{this.props.description}</p>
                 </div>
             </div>
         );
