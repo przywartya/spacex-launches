e2cd804e3dc698fa7db7ce77a9bfcdedd84fe8b1
ok <przywarty.it@gmail.com>
2018-05-18 23:18:11 +0200

    Handle errors and waiting.

--- src/view/LaunchesList.js
+++ src/view/LaunchesList.js
@@ -8,7 +8,7 @@ import Moon from '../assets/moon.png';
 import Footer from '../components/Footer';
 import FilterButtons from '../components/LaunchesList/FilterButtons';
 import Timeline from '../components/LaunchesList/Timeline';
-
+import { CircleLoader } from 'react-spinners';
 
 class LaunchesList extends React.Component {
   static propTypes = {
@@ -19,6 +19,8 @@ class LaunchesList extends React.Component {
   state = {
     rocketNameFilter: "FALCON 1",
     filteredLaunches: [],
+    isLoading: false,
+    error: null,
   };
 
   async componentDidMount() {
@@ -35,11 +37,14 @@ class LaunchesList extends React.Component {
     try {
       const rocketId = rocketName.split(" ").join("").toLowerCase();
       const URL = `https://api.spacexdata.com/v2/launches?rocket_id=${rocketId}`;
+      this.setState({ isLoading: true });
       const fetchResult = fetch(URL);
       const response = await fetchResult;
-      return await response.json();
-    } catch(e) {
-      throw Error(e);
+      const jsonResponse = await response.json();
+      this.setState({ isLoading: false });
+      return jsonResponse;
+    } catch(error) {
+      this.setState({ error, isLoading: false })
     }
   }
 
@@ -65,12 +70,18 @@ class LaunchesList extends React.Component {
           <img src={Moon} className="launches-list__moon"/>
           <Logo className="launches-list__logo"/>
           <h2>LAUNCHES 2018</h2>
-          <FilterButtons
+          <FilterButtons  
           options={this.availableRocketNames}
           onChange={this.handleFilterChange.bind(this)}/>
-          <Timeline 
-          filteredLaunches={this.state.filteredLaunches}
-          onLaunchClick={this.props.onLaunchClick}/>
+          { this.state.error !== null ? <h3>Connecting with SpaceX API failed. Please try again later.</h3> :
+            this.state.isLoading ? 
+            <CircleLoader color={'#ffffff'} loading={this.state.isLoading}/> :
+            this.state.filteredLaunches.length <= 0 ?
+            <h3>Sorry, no launches found.</h3> : 
+            <Timeline 
+            filteredLaunches={this.state.filteredLaunches}
+            onLaunchClick={this.props.onLaunchClick}/>
+          }
         </div>
         <div className="launches-list__footer">
           <Footer/>
